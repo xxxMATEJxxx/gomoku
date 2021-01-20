9513b11fdcfafa63520e8599e8374956baf1d693
HanaSvecova <hanita@atlas.cz>
2021-01-20 12:58:36 +0100

    nic moc

--- svecova/player.py
+++ svecova/player.py
@@ -11,15 +11,15 @@ class Player:
     self.round = 0
   def play(self, opponent_move):
     if opponent_move == None:
-        r = randint(0,14)
-        c = randint(0,14)
+        r = randint(4,10)
+        c = randint(4,10)
         self.r = r
         self.c = c
         return(r, c)
         
     row, col = opponent_move
     self.grid[row, col] = -self.sign
-    if self.round < 2:                             # pokud je počet kol menší než dva
+    if self.round < 0:                             # pokud je počet kol menší než dva
       if col < 14 and self.grid[row, col+1] == 0:  # pokud je v pravo od posledního tahu oponenta místo a zároveň nejsme na kraji
         print((row, col))
         self.round = self.round + 1                # počet kol se o jedno zvýší
@@ -32,45 +32,57 @@ class Player:
         self.c = col-1
         return(row, col-1)                         # křížek se nakreslí vlevo
     else:
-      if self.grid[self.r+1, self.c] == 0 and self.r < 14: # pokud je místo pod posledním naším umístěním,
+      if self.r < 14 and self.grid[self.r+1, self.c] == 0: # pokud je místo pod posledním naším umístěním,
         r = self.r+1
         c = self.c
         self.r = self.r+1
         self.c = self.c
         return(r, c)                                       # nakreslí se křížek pod posledním naším umístěním
-      elif self.grid[self.r+1, self.c+1] == 0 and self.r < 14 and self.c < 14:
+      elif self.r > 0 and self.grid[self.r-1, self.c] == 0:
+        r = self.r-1
+        c = self.c
+        self.r = self.r-1
+        self.c = self.c
+        return(r, c)                                       # nahoru o 1
+      elif self.r > 0 and self.grid[self.r-1, self.c] == 1:  #???
+        r = self.r-2
+        c = self.c
+        self.r = self.r-2
+        self.c = self.c
+        return(r, c)                                       # nahoru o 2
+      elif self.r > 0 and self.grid[self.r-2, self.c] == 1:  #???
+        r = self.r-3
+        c = self.c
+        self.r = self.r-3
+        self.c = self.c
+        return(r, c)                                       # nahoru o 3
+      elif self.r < 14 and self.c < 14 and self.grid[self.r+1, self.c+1] == 0:
         r = self.r+1
         c = self.c+1
         self.r = self.r+1
         self.c = self.c+1
         return(r, c)                                       # vpravo dole
-      elif self.grid[self.r+1, self.c-1] == 0 and self.r < 14 and self.c >0:
+      elif self.r < 14 and self.c >0 and self.grid[self.r+1, self.c-1] == 0:
         r = self.r+1
         c = self.c-1
         self.r = self.r+1
         self.c = self.c-1
         return(r, c)                                       # vlevo dole
-      elif self.grid[self.r-1, self.c] == 0 and self.r > 0:
-        r = self.r-1
-        c = self.c
-        self.r = self.r-1
-        self.c = self.c
-        return(r, c)                                       # nahoře
-      elif self.grid[self.r-1, self.c-1] == 0 and self.r > 0 and self.c > 0:
+      elif self.r > 0 and self.c > 0 and self.grid[self.r-1, self.c-1] == 0:
         r = self.r-1
         c = self.c-1
         self.r = self.r-1
         self.c = self.c-1
         return(r, c)                                       # vlevo nahoře
-      elif self.grid[self.r-1, self.c-1] == 0 and self.r > 0 and self.c < 14:
+      elif self.r > 0 and self.c < 14 and self.grid[self.r-1, self.c-1] == 0:
         r = self.r-1
         c = self.c+1
         self.r = self.r-1
         self.c = self.c+1
         return(r, c)                                       # vpravo nahoře
       else:
-        r = randint(0,14)
-        c = randint(0,14)
+        r = randint(4,10)
+        c = randint(4,10)
         self.r = r
         self.c = c
         return(r, c)
