382263eccc8e90372ef27ec609329aa97f58f2d7
Dihydroxyaceton <73236021+Dihydroxyaceton@users.noreply.github.com>
2021-01-30 17:28:43 +0100

    works quite well

--- sebastian/player.py
+++ sebastian/player.py
@@ -1,79 +1,173 @@
-from random import randint
+import random
 
-opponent_occupied_rows = []
-opponent_occupied_cols = []
-player_occupied_rows = []
-player_occupied_cols = []
+PATTERNS = [
+    (1000000000000, 'xxxxx'),
+    (-10000000000000, 'ooooo'),
+    
+    (100000000, 'xxxx '),    # X X X X _
+    (-1000000000, 'oooo '),   # O O O O _
+    
+    
+    (100000000, ' xxxx'),    # _ X X X X
+    (-1000000000, ' oooo'),   # _ O O O O
+
+    (-100000000, 'oo oo'),   # O O _ O O
+
+    (1000000, '  xxx  '),  # _ _ X X X _ _
+    (-1000000, '  ooo  '), # _ _ O O O _ _ 
+    
+    (-100, ' o o '),    
+    
+    (10, '   xx   '), # _ _ _ X X _ _ _
+    (-10, '   oo   '),# _ _ _ O O _ _ _ 
+
+    (1000, 'xooox'),
+    (1000, 'xooox'),
+    (1000, 'xooo'),
+    (1000, 'ooox'),
+    
+    (-10000000, ' oo o'),
+    (-10000000, 'oo o '),
+    (-10000000, ' o oo'),
+    (-10000000, 'o oo ')
+    
+
+]
+class Board:
+    SIZE = 15
+
+    def generate_rows(self):
+        rows = []
+        for i in range(self.SIZE):
+            row = []
+            for j in range(self.SIZE):
+                row.append(0)
+            rows.append(row)
+        return rows
+
+    def generate_diagonals(self):
+        diagonals = []
+        delka = 1
+        for i in range(self.SIZE):
+            diagonal = []
+            for j in range(delka):
+                diagonal.append(0)
+            diagonals.append(diagonal)
+            delka += 1
+        delka = 14
+        for i in range(self.SIZE - 1):
+            diagonal = []
+            for j in range(delka):
+                diagonal.append(0)
+            diagonals.append(diagonal)
+            delka -= 1
+        return diagonals
+
+    def __init__(self):
+        self.rows = self.generate_rows()
+        self.columns = self.generate_rows()
+        self.diagonals_descending = self.generate_diagonals()
+        self.diagonals_ascending = self.generate_diagonals()
+
+    def row_to_string(self, row):
+        output = ''
+        for i in row:
+            if (i == 0):
+                output += ' '
+            if (i == 1):
+                output += 'x'
+            if (i == -1):
+                output += 'o'
+        return output
+
+    def evaluate_row(self, row):
+        string_row = self.row_to_string(row)
+        total_score = 0
+        for pattern in PATTERNS:
+            score, p = pattern
+            if p in string_row:
+                print(f'found pattern {p} in {row}')
+                total_score += score
+        return total_score
+
+
+    def evaluate_position(self):
+        total_score = 0
+        for row in self.rows:
+            total_score += self.evaluate_row(row)
+        for col in self.columns:
+            total_score += self.evaluate_row(col)
+        for asc in self.diagonals_ascending:
+            total_score += self.evaluate_row(asc)
+        for desc in self.diagonals_descending:
+            total_score += self.evaluate_row(desc)
+        return total_score
+
+    def new_turn(self, row, column, player):
+        self.rows[row][column] = player
+        self.columns[column][row] = player
+        ascending_diagonal_number = row + column
+        if (row + column < self.SIZE):
+            self.diagonals_ascending[ascending_diagonal_number][column] = player
+        else:
+            self.diagonals_ascending[ascending_diagonal_number][self.SIZE - 1 - row] = player
+        descending_diagonal_number = self.SIZE - 1 - row + column
+        if (descending_diagonal_number < 15):
+            self.diagonals_descending[descending_diagonal_number][column] = player
+        else:
+            self.diagonals_descending[descending_diagonal_number][row] = player
+        #self.print_all()
+
+    def get(self, row, col):
+        return self.rows[row][col]
+
+    def print_all(self):
+        print('rows')
+        for row in self.rows:
+            print(row)
+        print('cols')
+        for col in self.columns:
+            print(col)
+        print('desc')
+        for d in self.diagonals_descending:
+            print(d)
+        print('asc')
+        for d in self.diagonals_ascending:
+            print(d)
 
 class Player:
-	def __init__(self, player_sign):
-		self.sign = player_sign
-		self.name = 'example'
-	def play(self, opponent_move):
-		#if opponent_move == None:
-		#	return (7, 7)			# FIRST MOVE
-		#ok_move_opponent_occ = 0
-		#ok_move_player_occ = 0
-		
-		row, col = opponent_move
-		print (opponent_move)
-	"""
-		
-		opponent_occupied_rows.append(row)
-		opponent_occupied_cols.append(col)
-		
-		dest_row = row + randint(-3,3)
-		dest_col = col + randint(-3,3)
+    def __init__(self, player_sign):
+        self.sign = 1
+        self.opponent_sign = -1
+        self.name = 'Sebastian bot'
+        self.board = Board()
+        random.seed(17)
 
-		for coord in opponent_occupied_rows:
-			if dest_row == coord:
-				ok_move_opponent_occ += 0
-			else:
-				ok_move_opponent_occ += 1
-		
-		for coord in opponent_occupied_cols:
-			if dest_col == coord:
-				ok_move_opponent_occ += 0
-			else:
-				ok_move_opponent_occ += 1
-		
-		print (opponent_occupied_rows)
-		print (opponent_occupied_cols)
-		
-		for coord in player_occupied_cols:
-			if dest_col == coord:
-				ok_move_player_occ += 0
-			else:
-				ok_move_player_occ += 1
-		
-		for coord in player_occupied_cols:
-			if dest_col == coord:
-				ok_move_player_occ += 0
-			else:
-				ok_move_player_occ += 1	
-		
-		print (player_occupied_rows)
-		print (player_occupied_cols)
-		print ("opp field score: "+str(ok_move_opponent_occ))
-		print ("player field score: "+str(ok_move_player_occ))
-		
-		
-		if ok_move_opponent_occ >= 1 or ok_move_player_occ >= 1:
-			if row >= 0 and row <= 15:
-				if col >= 0 and col <= 15:
-					player_occupied_rows.append(row)
-					player_occupied_cols.append(col)
-					ok_move_opponent_occ = 0
-					ok_move_player_occ = 0
-					return (dest_row, dest_col)
-				else:
-					print("INVALID MOVE - placed outside the grid")
-					play(self, opponent_move)
-			else:
-				print("INVALID MOVE - placed outside the grid")
-				play(self, opponent_move)
-		else:
-			print("INVALID MOVE - placed on an occupied field")
-			play(self, opponent_move)
-		"""
+    def pick_random_valid_turn(self):
+        while True:
+            row = random.randint(0, 14)
+            col = random.randint(0, 14)
+            if (self.board.get(row, col) == 0): return (row, col)
 
+    def pick_best_turn(self):
+        best_score = -float('inf')
+        best_turn = None
+        for row in range(15):
+            for col in range(15):
+                if (self.board.get(row, col) != 0): continue
+                self.board.new_turn(row, col, self.sign)
+                score = self.board.evaluate_position()
+                if score > best_score:
+                    best_turn = (row, col)
+                    best_score = score
+                self.board.new_turn(row, col, 0)
+        return best_turn
+
+    def play(self, opponent_move):
+        if opponent_move != None:
+            row, col = opponent_move
+            self.board.new_turn(row, col, self.opponent_sign)
+        #my_turn_row, my_turn_col = self.pick_random_valid_turn()
+        my_turn_row, my_turn_col = self.pick_best_turn()
+        self.board.new_turn(my_turn_row, my_turn_col, self.sign)
+        return my_turn_row, my_turn_col
