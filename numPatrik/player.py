7d2d355aa1b41fc4746f7125ac41b4323bd6961a
Patrik Černohorský <73184928+TheBigGandalf@users.noreply.github.com>
2021-03-03 10:41:14 +0100

    Update player.py
    vylepšení

--- numPatrik/player.py
+++ numPatrik/player.py
@@ -1,40 +1,49 @@
-import numpy as np
+import random
 
+PATTERNS = [
+    (10000000000000000, 'xxxxx'),
+    (-1000000000000000, 'ooooo'),
+
+    (100000000000, ' xxxx '),
+    (-10000000000, ' oooo '),
+
+    (-10000000000, 'oxxxx '),
+    (-10000000000, ' xxxxo'),
+    (-100000000, 'xoooo '),
+    (-100000000, ' oooox'),
+
+    (1000000000, 'xx xx'),
+    (-100000000, 'oo oo'),
+
+    (-10000000, ' oo o'),
+    (-10000000, 'oo o '),
+    (-10000000, ' o oo'),
+    (-10000000, 'o oo '),
+    (1000, 'xooox'),
+    (1000, 'xooox'),
+    (10000, 'xooo'),
+    (10000, 'ooox'),
+    (-100000000, 'xx  o'),
+    (-100000000, 'o  xx'),
+
+    (1000000, '  xxx  '),
+    (-1000000, '  ooo  '),
+    (-100, ' o o '),
+    (10, '   xx   '),
+    (-10, '   oo   '),
+    (1, '       x       ')
+]
 class Board:
-    def __init__(self):
-        self.SIZE = 15
-        self.rows = np.zeros((15, 15), dtype="i")
-        self.columns = np.zeros((15, 15), dtype="i")
-        self.diagonals_asc = self.generate_diagonals()
-        self.diagonals_disc = self.generate_diagonals()
-        self.sets = {
-                    "010": 5,
-                     "0110": 50,
-                     "01110": 100,
-                     "11110": 150,
-                     "01111": 151,
-                     "011110": 152,
-                     "210":3,
-                     "2110": 10,
-                     "21110" :40,
-                     "211110" : 153,
-                     "2" : 1,
-                     "012": 2,
-                     "0112": 11,
-                     "01112": 40,
-                     "011112": 154,
-                     "22220": 140,
-                     "02222": 141,
-                     "0220": 49,
-                     "122220": 142,
-                     "022221" : 143,
-                     "02220": 99,
+    SIZE = 15
 
-                     }
-
-        self.sign = 1
-        self.opponent_sign = 2
-
+    def generate_rows(self):
+        rows = []
+        for i in range(self.SIZE):
+            row = []
+            for j in range(self.SIZE):
+                row.append(0)
+            rows.append(row)
+        return rows
 
     def generate_diagonals(self):
         diagonals = []
@@ -52,110 +61,122 @@ class Board:
                 diagonal.append(0)
             diagonals.append(diagonal)
             delka -= 1
+        print(diagonals)
         return diagonals
 
-    def checkingOP (self):
-        story = ""
-        choices = []
-        BOARD = [self.rows, self.columns, self.diagonals_asc, self.diagonals_disc]
-        for type in BOARD:
-            for row in type:
-                for char in row:
-                    story += str(char)
-                story += ","
+    def __init__(self):
+        self.rows = self.generate_rows()
+        self.columns = self.generate_rows()
+        self.diagonals_descending = self.generate_diagonals()
+        self.diagonals_ascending = self.generate_diagonals()
+        print(self.diagonals_descending)
+
+    def row_to_string(self, row):
+        output = ''
+        for i in row:
+            if (i == 0):
+                output += ' '
+            if (i == 1):
+                output += 'x'
+            if (i == -1):
+                output += 'o'
+        return output
+
+    def evaluate_row(self, row):
+        string_row = self.row_to_string(row)
+        total_score = 0
+        for pattern in PATTERNS:
+            score, p = pattern
+            if p in string_row:
+                #print(f'found pattern {p} in {row}')
+                total_score += score
+                #total_score = total_score + score
+        return total_score
 
 
-        for combination in self.sets.keys():
-            comPlace =story.find(combination)
-            if comPlace > 0:
-                choices.append((comPlace, combination, self.sets[combination]))
+    def evaluate_position(self):
+        total_score = 0
+        for row in self.rows:
+            total_score += self.evaluate_row(row)
+
+        for column in self.columns:
+            total_score += self.evaluate_row(column)
+
+        for asc in self.diagonals_ascending:
+            total_score += self.evaluate_row(asc)
+
+        for disc in self.diagonals_descending:
+            total_score += self.evaluate_row(disc)
 
 
-        best = (88,"0",0) # pozměnit tak, aby na zač dávalo nějaké pěkné souřadnice
-        for chance in choices:
-            if best[2] < chance[2]:
-                best = chance
-
-        print(best)
-        return self.placing(best)
-
-    def placing(self, best):
-        print(best)
-        thePlace = best[0] + best[1].find("0")
-        if thePlace < 480:
-            if thePlace > 240: thePlace -= 240
-            self.y = thePlace // 16
-            self.x = thePlace % 16
-        else: #diagonals
-            p = -1 #souřadnice od nuly
-            asc_dis = 1
-            thePlace -= 480
-            if thePlace < 135 or  254< thePlace < 389 :
-                if thePlace < 135: asc_dis = -1
-                for pp in range (2,17):
-                    p += pp
-                    if thePlace < p:
-                        self.x = thePlace - p - 1
-                        self.y = pp - 3 + self.x * asc_dis #protože čárky, zač. 1, a protoře číslo se nachzí o jednu řadu níž
-
-            elif 135 < thePlace < 254 or thePlace > 389: # když se to nachází v druhé polovině mezi zmenšujími se
-                row = 0
-                if thePlace > 389:
-                    thePlace -= 389
-                    asc_dis = 0
-                else: thePlace -= 135
-                for pp in range (2,16):
-                    pp = 16-pp
-                    row += 1
-                    p += pp
-                    if thePlace < p:
-
-                        self.y = 15*asc_dis - thePlace -1
-                        self.x = row + thePlace
-
-        return (self.y, self.x)
-
-
-
-
-        #převedení best/chance na souřadnic vhodného tahu a vrácení jich
+        return total_score
 
     def new_turn(self, row, column, player):
         self.rows[row][column] = player
         self.columns[column][row] = player
         ascending_diagonal_number = row + column
         if (row + column < self.SIZE):
-            self.diagonals_asc[ascending_diagonal_number][column] = player
+            self.diagonals_ascending[ascending_diagonal_number][column] = player
         else:
-            self.diagonals_asc[ascending_diagonal_number][self.SIZE - 1 - row] = player
+            self.diagonals_ascending[ascending_diagonal_number][self.SIZE - 1 - row] = player
         descending_diagonal_number = self.SIZE - 1 - row + column
         if (descending_diagonal_number < 15):
-            self.diagonals_disc[descending_diagonal_number][column] = player
+            self.diagonals_descending[descending_diagonal_number][column] = player
         else:
-            self.diagonals_disc[descending_diagonal_number][row] = player
+            self.diagonals_descending[descending_diagonal_number][row] = player
+        #self.print_all()
 
+    def get(self, row, col):
+        return self.rows[row][col]
 
-
-
-
-b = Board()
-b.checkingOP()
-
-
+    def print_all(self):
+        print('rows')
+        for row in self.rows:
+            print(row)
+        print('cols')
+        for col in self.columns:
+            print(col)
+        print('desc')
+        for d in self.diagonals_descending:
+            print(d)
+        print('asc')
+        for d in self.diagonals_ascending:
+            print(d)
 
 class Player:
     def __init__(self, player_sign):
         self.sign = 1
-        self.opponent_sign = 2
-        self.name = 'Patrik'
+        self.opponent_sign = -1
+        self.name = 'Patrik2'
         self.board = Board()
+        random.seed(17)
+
+    """
+        def pick_random_valid_turn(self):
+            while True:
+                row = random.randint(0, 14)
+                col = random.randint(0, 14)
+                if (self.board.get(row, col) == 0): return (row, col)
+    """
+    def pick_best_turn(self):
+        best_score = (-float('inf'))
+        best_turn = None
+        for row in range(15):
+            for col in range(15):
+                if (self.board.get(row, col) != 0): continue
+                self.board.new_turn(row, col, self.sign)
+                score = self.board.evaluate_position()
+                if score > best_score:
+                    best_turn = (row, col)
+                    best_score = score
+                self.board.new_turn(row, col, 0)
+        return best_turn
 
     def play(self, opponent_move):
         if opponent_move != None:
             row, col = opponent_move
             self.board.new_turn(row, col, self.opponent_sign)
-
-        my_turn_row, my_turn_col = self.board.checkingOP()
+        #my_turn_row, my_turn_col = self.pick_random_valid_turn()
+        my_turn_row, my_turn_col = self.pick_best_turn()
         self.board.new_turn(my_turn_row, my_turn_col, self.sign)
-        print(self.board.rows, self.board.columns, self.board.diagonals_asc, self.board.diagonals_disc)
         return my_turn_row, my_turn_col
